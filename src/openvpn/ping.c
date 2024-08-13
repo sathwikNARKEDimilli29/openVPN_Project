eea3ae9cb505aeabe6f4de2ada983801e48af315
sathwik narkedimilli <97584810+sathwikNARKEDimilli29@users.noreply.github.com>
2024-08-13 13:46:14 +0530

    Update ping.c

--- src/openvpn/ping.c
+++ src/openvpn/ping.c
@@ -1,26 +1,3 @@
-/*
- *  OpenVPN -- An application to securely tunnel IP networks
- *             over a single TCP/UDP port, with support for SSL/TLS-based
- *             session authentication and key exchange,
- *             packet encryption, packet authentication, and
- *             packet compression.
- *
- *  Copyright (C) 2002-2024 OpenVPN Inc <sales@openvpn.net>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2
- *  as published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -30,7 +7,7 @@
 #include "ping.h"
 
 #include "memdbg.h"
-#include <unistd.h>  // Include for sleep function
+#include <unistd.h>  // Include for sleep and usleep functions
 
 /*
  * This random string identifies an OpenVPN ping packet.
@@ -50,7 +27,7 @@ trigger_ping_timeout_signal(struct context *c)
     struct gc_arena gc = gc_new();
     
     // Introduce a delay (timeout) to defeat fingerprinting technique
-    sleep(5);  // Sleep for 5 seconds (or any appropriate time as required)
+    usleep(50000);  // Sleep for 50 milliseconds (50,000 microseconds)
 
     switch (c->options.ping_rec_timeout_action)
     {
@@ -79,7 +56,7 @@ void
 check_ping_send_dowork(struct context *c)
 {
     // Introduce a delay to match the timeout delay in the timeout signal
-    sleep(5);  // Sleep for 5 seconds (or any appropriate time as required)
+    usleep(50000);  // Sleep for 50 milliseconds (50,000 microseconds)
 
     c->c2.buf = c->c2.buffers->aux_buf;
     ASSERT(buf_init(&c->c2.buf, c->c2.frame.buf.headroom));
